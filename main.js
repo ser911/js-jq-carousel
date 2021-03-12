db988d74a5c14b687262552ffda801b28e73aa30
Sergio <sergio.marretta@libero.it>
2021-03-12 16:55:16 +0100

    broken

--- main.js
+++ main.js
@@ -9,17 +9,20 @@ $(".prev").click(
 $(".next").click(
 function(){
 var activeImg = $(".images img.active");
-}
 if (activeImg.hasClass("last") == true) {
+
+
+}
   $(".images img.first").addClass("active");
 }
+
 
 activeImg.removeClass("active");
 activeImg.next().addClass("active");
 )
 
 
-
+}
 
 
 
